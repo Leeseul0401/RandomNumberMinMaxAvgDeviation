6f7a53c6d896b1012c3e491fb02abbb9acb87542
leeseul <7836764@naver.com>
2024-05-10 17:24:46 +0900

    initial commit

---
+++ MFCApplication1Dlg.h
@@ -0,0 +1,53 @@
+﻿
+// MFCApplication1Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCApplication1Dlg 대화 상자
+class CMFCApplication1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplication1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedAvgBtn();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedRandomNumberBtn();
+	void OnEnChangeAvgEdit();
+	CEdit inputNumberEdit;
+	CEdit avgEdit;
+	CListBox listBox;
+	void GetAverage(double* data, int size, double* average);
+	void GetStandardDeviation(double* array, int size, double* standardDeviation);
+	void GetFindMinMax(double* data, int size, double & minValue, double & maxValue);
+	CString avgEditValue;
+	afx_msg void OnBnClickedStandardDeviationBtn();
+	CEdit standardDevationEdit;
+	afx_msg void OnBnClickedMinMaxBtn();
+	afx_msg void OnEnChangeEdit4();
+	void OnBnClickedResetBtn();
+	CEdit minMaxEdit;
+	std::vector<int> vec;
+};
